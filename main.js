9b3f06aa66ee1c30af4e15f48aa0713b17f058cf
cyril572 <nnaemekacyril2022@gmail.com>
2025-06-15 02:13:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let x=document.getElementById("menu")
+let mee=document.querySelector("mee")
+function openMenu(){
+   x.style.display="block"
+}
+ dark.add.EventListener("click", darkMode)
+ function darkMode(){
+   document.body.style.background="black"
+   document.body.style.color="white"
+   document.header.style.background="black"
+   alert("dark mode activated")
+ }
+   let y=document.getElementById("reg")
+  reg =document.addEventListener("click" ,opMenu )
+  function opMenu(){
+    reg.style.display="block"
+    
+    if(reg.style.display=="block"){
+      alert("Pls fill the input to register")
+    }
+  }
+  
+    let p=document.getElementById("part")
+    part=document.addEventListener("click",lastMenu ) 
+    function lastMenu(){
+      part.style.display="block"
+      alert('thanks for registering to mac_fragrance where u can buy luxry perfume etc'+"smell good is our priority" )
+    }
+      let a=document.getElementById("name2")
+      name2=document.addEventListener("click" ,submit3)
+    function submit3(){
+        alert("registered sucessfull")
+    }
